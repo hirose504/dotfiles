14bf2dde33062161550c09bd279796293f0f9984
hirose504 <hirose504@yahoo.co.jp>
2014-02-05 02:06:14 +0900

    fix display of tmux pwd and weather segments

--- .tmux/tmux-powerline-settings/themes/default.sh
+++ .tmux/tmux-powerline-settings/themes/default.sh
@@ -29,25 +29,25 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		#"ifstat_sys 30 255" \
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		#"wan_ip 24 255" \
-		#"vcs_branch 29 88" \
-		#"vcs_compare 60 255" \
-		#"vcs_staged 64 255" \
-		#"vcs_modified 9 255" \
-		#"vcs_others 245 0" \
+		"vcs_branch 29 88" \
+		"vcs_compare 60 255" \
+		"vcs_staged 64 255" \
+		"vcs_modified 9 255" \
+		"vcs_others 245 0" \
 	)
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		#"pwd 89 211" \
+		"pwd 89 211" \
 		#"mailcount 9 255" \
 		#"now_playing 234 37" \
 		#"cpu 240 136" \
-		"load 1 0" \
+		"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
 		#"battery 137 127" \
-		#"weather 37 255" \
+		"weather 37 255" \
 		#"rainbarf 0 0" \
 		#"xkb_layout 125 117" \
 		"date_day 5 0" \
--- .zshrc
+++ .zshrc
@@ -33,3 +33,5 @@ esac
 # Tell antigen that you're done.
 antigen apply
 
+PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")'
+
